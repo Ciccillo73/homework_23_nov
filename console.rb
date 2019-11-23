dc4888db6d3aa02d95a401fa472fd697dcdecd69
Francesco <med.dama@gmail.com>
2019-11-23 17:59:42 +0000

    making up the pry file

---
+++ console.rb
@@ -0,0 +1,13 @@
+require_relative('..models/customer')
+require_relative('..models/film')
+require_relative('..models/ticket')
+
+require('psy-byebug')
+
+Film.delete_all()
+
+film1 = Film.new({ 'title' => 'Scarface', 'price' => 5.00})
+film1.save()
+
+binding.pry
+nil
