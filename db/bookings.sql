2ca5a81d4386bfd8e8825b8a232d61e7ca82c0d1
Francesco <med.dama@gmail.com>
2019-11-23 12:51:50 +0000

    Creating database file tables and create database

---
+++ db/bookings.sql
@@ -0,0 +1,23 @@
+DROP TABLE tickets;
+DROP TABLE films;
+DROP TABLE customers;
+
+CREATE TABLE films (
+  id SERIAL4 PRIMARY KEY,
+  title VARCHAR(255),
+  price DEC,
+);
+
+CREATE TABLE customers (
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  funds DEC,
+);
+
+CREATE TABLE tickets (
+  id SERIAL4 PRIMARY KEY,
+  customer_id INT4 REFERENCES customers(id) ON DELETE CASCADE,
+  film_id INT4 REFERENCES films(id) ON DELETE CASCADE,
+  review TEXT
+
+);
